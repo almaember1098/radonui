797a24b80c2f38222707e24b49477fe7c234b63e
NONAME-657 <adamhamori2@gmail.com>
2019-05-16 21:02:13 +0200

    Beginned implementing components.

---
+++ radonjs/radon-components.js
@@ -0,0 +1,45 @@
+// the component manager class
+// it manages components
+
+class ComponentManager {
+    static _components={}
+
+    // creates a new component
+    // example:
+    // ComponentManager.createComponent('hello', new StaticComponent(null, null))
+    static createCompnent(name, object) {
+        this._components[name]=object
+    }
+}
+
+
+// the RadonJS component class
+// this is the base class of all
+// RadonJS components
+
+class RadonComponent {
+
+    // the name of the component
+    _componentName
+    // the template
+    _template
+    constructor(componentName, template) {
+        this._componentName=componentName
+        this._template=template
+    }
+}
+
+
+// static component baseclass
+
+class StaticComponent extends RadonComponent {
+    constructor(componentName, template) {
+        super(componentName, template)
+    }
+
+    // simply returns the template.
+    // and we will not use the attr parameter
+    render(attr) {
+        return this._template
+    }
+}
