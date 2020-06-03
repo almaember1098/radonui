392920a51e310d23732983cc5a59d19cc94aabd0
almaember1098 <hamoriadam178@gmail.com>
2020-06-03 23:19:01 +0200

    Added renderComponents which can render every component in code

--- radonui/radon-components.js
+++ radonui/radon-components.js
@@ -46,3 +46,29 @@ class StaticComponent extends RadonComponent {
         return this._template
     }
 }
+
+// renders all and every component(s)
+function renderComponents() {
+    // names for the attributes helping with the rendering
+    const attrNameComp="rn-render"
+    const attrNameModel="rn-model"
+    // get all elements in <body>
+    var elements=document.getElementsByTagName("*")
+    // iterate over the elements
+    Array.prototype.filter.call(elements, function(el) {
+        var doRender=el.hasAttribute(attrNameComp)
+        var hasModel=el.hasAttribute(attrNameModel)
+
+        if(doRender) {
+            // render the component
+            var compName=el.getAttribute(attrNameComp)
+            var modelName
+            if(hasModel) {
+                modelName=el.getAttribute(attrNameModel)
+            } else {
+                modelName=null
+            }
+            el.innerHTML=ComponentManager.render(compName, eval(modelName))
+        }
+    })
+}
